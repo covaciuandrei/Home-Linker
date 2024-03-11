11ac764ca054d820263c021e0f0ca6977cbcabfe
Covaciu Andrei <covaciuandrei21@gmail.com>
2024-03-11 20:29:20 +0200

    #7 put the wigets only used once back in the file they are used

--- lib/presentation/pages/property/property_page.dart
+++ lib/presentation/pages/property/property_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:homelinker/models/property.dart';
 import 'package:homelinker/presentation/widgets/back_arrow_button.dart';
 import 'package:homelinker/presentation/widgets/listing_price.dart';
-import 'package:homelinker/presentation/widgets/property_sellling_point.dart';
 import 'package:homelinker/utils/extension_methods.dart';
 
 @RoutePage()
@@ -129,62 +128,7 @@ class PropertyPage extends StatelessWidget {
                           ],
                           isFirstLine: false,
                         ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 4,
-                            horizontal: 20,
-                          ),
-                          child: const Column(
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.only(bottom: 4),
-                                        child: Text(
-                                          'Description',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color:
-                                                Color.fromRGBO(28, 83, 119, 1),
-                                            fontSize: 20,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Text(
-                                    '   Charming apartment nestled in a vibrant neighborhood, offering modern comforts and convenience. This cozy space features a spacious bedroom, a well-equipped kitchen, and a stylish living area with ample natural light. ',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromRGBO(20, 112, 161, 1),
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  Text(
-                                    '   Charming apartment nestled in a vibrant neighborhood, offering modern comforts and convenience. This cozy space features a spacious bedroom, a well-equipped kitchen, and a stylish living area with ample natural light. ',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromRGBO(20, 112, 161, 1),
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  Text(
-                                    '   Charming apartment nestled in a vibrant neighborhood, offering modern comforts and convenience. This cozy space features a spacious bedroom, a well-equipped kitchen, and a stylish living area with ample natural light. ',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromRGBO(20, 112, 161, 1),
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
+                        const PropertyDescription(),
                       ],
                     ),
                   ),
@@ -193,6 +137,162 @@ class PropertyPage extends StatelessWidget {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class PropertyDescription extends StatelessWidget {
+  const PropertyDescription({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 4,
+        horizontal: 20,
+      ),
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 4),
+                child: Text(
+                  'Description',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(28, 83, 119, 1),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Text(
+            '   Charming apartment nestled in a vibrant neighborhood, offering modern comforts and convenience. This cozy space features a spacious bedroom, a well-equipped kitchen, and a stylish living area with ample natural light. ',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(20, 112, 161, 1),
+              fontSize: 14,
+            ),
+          ),
+          Text(
+            '   Charming apartment nestled in a vibrant neighborhood, offering modern comforts and convenience. This cozy space features a spacious bedroom, a well-equipped kitchen, and a stylish living area with ample natural light. ',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(20, 112, 161, 1),
+              fontSize: 14,
+            ),
+          ),
+          Text(
+            '   Charming apartment nestled in a vibrant neighborhood, offering modern comforts and convenience. This cozy space features a spacious bedroom, a well-equipped kitchen, and a stylish living area with ample natural light. ',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(20, 112, 161, 1),
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PropertySellingPointLine extends StatelessWidget {
+  const PropertySellingPointLine({
+    super.key,
+    required this.property,
+    required this.icons,
+    required this.isFirstLine,
+  });
+
+  final Property property;
+  final List<IconData> icons;
+  final bool isFirstLine;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: MediaQuery.of(context).size.height * 0.1,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: [
+            PropertySellingPoint(
+              icon: icons[0],
+              text: isFirstLine
+                  ? 'Year ${property.constructionYear}'
+                  : '${property.bathrooms} ${property.bathrooms == 1 ? 'bathroom' : 'bathrooms'}',
+            ),
+            PropertySellingPoint(
+              icon: icons[1],
+              text: isFirstLine
+                  ? 'For ${property.listingType.name}'
+                  : '${property.bedrooms} ${property.bedrooms == 1 ? 'bedroom' : 'bedrooms'}',
+            ),
+            PropertySellingPoint(
+              icon: icons[2],
+              text: isFirstLine
+                  ? 'Size ${property.areaSize} m²'
+                  : '${property.parkingSpaces} ${property.parkingSpaces == 1 ? 'parking space' : 'parking spaces'}',
+            ),
+          ],
+        ));
+  }
+}
+
+class PropertySellingPoint extends StatelessWidget {
+  const PropertySellingPoint({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(4),
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFFE3EDF4),
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 6),
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                    color: Color(0xFFBFDCEF),
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Icon(
+                  icon,
+                  color: const Color.fromRGBO(20, 112, 161, 1),
+                ),
+              ),
+              Flexible(
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                    color: Color.fromRGBO(28, 83, 119, 1),
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
