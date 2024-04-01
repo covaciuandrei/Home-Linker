8521981a1e730c60559d385e6e2464e59d99468f
Covaciu Andrei <covaciuandrei21@gmail.com>
2024-04-02 02:03:09 +0300

    #15 improved main appbar and main button

--- lib/presentation/widgets/main_appbar.dart
+++ lib/presentation/widgets/main_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -5,22 +6,48 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
     super.key,
     this.title,
     this.color = const Color.fromRGBO(70, 179, 231, 1),
+    this.hasBackButton = true,
+    this.onBackButtonPressed,
   });
 
   final String? title;
   final Color color;
+  final bool hasBackButton;
+  final VoidCallback? onBackButtonPressed;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       iconTheme: const IconThemeData(color: Colors.white, size: 28),
       backgroundColor: color,
-      title: Text(
-        title ?? 'Settings',
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
+      leading: hasBackButton
+          ? Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    if (onBackButtonPressed != null) {
+                      onBackButtonPressed!();
+                    }
+                    AutoRouter.of(context).pop();
+                  },
+                );
+              },
+            )
+          : const Text(''),
+      title: Row(
+        children: [
+          Text(
+            title ?? '',
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/presentation/widgets/main_button.dart
+++ lib/presentation/widgets/main_button.dart
@@ -9,6 +9,7 @@ class MainButton extends StatelessWidget {
     this.height,
     this.icon,
     this.iconColor,
+    this.isEnabled = true,
   });
 
   final String text;
@@ -17,41 +18,45 @@ class MainButton extends StatelessWidget {
   final double? height;
   final IconData? icon;
   final Color? iconColor;
+  final bool isEnabled;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: width ?? double.infinity,
       height: height,
-      child: ElevatedButton(
-        style: ButtonStyle(
-          backgroundColor:
-              MaterialStateProperty.all(const Color.fromRGBO(250, 250, 250, 1)),
-        ),
-        onPressed: onPressed,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (icon != null)
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: Icon(
-                  icon,
-                  size: 24,
-                  color: iconColor ?? Colors.lightBlue,
+      child: IgnorePointer(
+        ignoring: !isEnabled,
+        child: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+            isEnabled ? const Color.fromRGBO(250, 250, 250, 1) : Colors.grey,
+          )),
+          onPressed: isEnabled ? onPressed : null,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (icon != null)
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Icon(
+                    icon,
+                    size: 24,
+                    color: iconColor ?? Colors.lightBlue,
+                  ),
+                ),
+              Flexible(
+                child: Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            Flexible(
-              child: Text(
-                text,
-                style: const TextStyle(
-                  color: Colors.lightBlue,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
