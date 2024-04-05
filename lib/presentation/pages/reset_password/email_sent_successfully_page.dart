7219b2b7c25661656f0c325d682794d4f8c8b18d
Covaciu Andrei <covaciuandrei21@gmail.com>
2024-04-06 00:22:47 +0300

    #17 created email sent successfully page

---
+++ lib/presentation/pages/reset_password/email_sent_successfully_page.dart
@@ -0,0 +1,47 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:homelinker/core/app_router.gr.dart';
+import 'package:homelinker/presentation/widgets/blue_shadow_background.dart';
+import 'package:homelinker/presentation/widgets/main_button.dart';
+
+@RoutePage()
+class EmailSentSuccessfullyPage extends StatelessWidget {
+  const EmailSentSuccessfullyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
+      body: Center(
+        child: BlueShadowBackground(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 250),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Flexible(
+                  child: Text(
+                    'Mail-ul a fost trimis cu succes.',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 36),
+                MainButton(
+                  width: 140,
+                  text: 'Gata',
+                  onPressed: () {
+                    AutoRouter.of(context).pushAndPopUntil(
+                      const IntroductiveRoute(),
+                      predicate: (route) => false,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
